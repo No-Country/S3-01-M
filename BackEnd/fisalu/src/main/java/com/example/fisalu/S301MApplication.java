6c57aaabc97fa10bb3477f37e37badd6ad9a025a
julianRosales <rjulianbenjamin@gmail.com>
2022-08-05 15:09:52 -0300

    Creacion

---
+++ BackEnd/fisalu/src/main/java/com/example/fisalu/S301MApplication.java
@@ -0,0 +1,13 @@
+package com.example.fisalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S301MApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S301MApplication.class, args);
+	}
+
+}
---
+++ BackEnd/fisalu/src/test/java/com/example/fisalu/S301MApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fisalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S301MApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
