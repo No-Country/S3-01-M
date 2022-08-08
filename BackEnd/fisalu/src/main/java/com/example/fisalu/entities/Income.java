b090d7abc3e3b20d78e059acdaf875a5cc6d6a05
Chelso <celsobrc@gmail.com>
2022-08-08 14:43:11 -0300

    Income entity added

---
+++ BackEnd/fisalu/src/main/java/com/example/fisalu/entities/Income.java
@@ -0,0 +1,44 @@
+package com.example.fisalu.entities;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "incomes")
+@SQLDelete(sql = "UPDATE incomes SET deleted = true WHERE id=?")
+@Where(clause = "deleted = false")
+@Data
+public class Income implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "mount")
+    private Double mount;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "deleted")
+    private boolean deleted = Boolean.FALSE;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDate updatedAt;
+
+}
