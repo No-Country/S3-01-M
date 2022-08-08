1976b243a98203cf39a06932f9ce9226c7233222
Chelso <celsobrc@gmail.com>
2022-08-08 18:07:00 -0300

    Save endpoint added to income controller

---
+++ BackEnd/fisalu/src/main/java/com/example/fisalu/controllers/IncomeController.java
@@ -0,0 +1,21 @@
+package com.example.fisalu.controllers;
+
+import com.example.fisalu.dtos.IncomeDto;
+import com.example.fisalu.services.IncomeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/incomes")
+public class IncomeController {
+
+    private static IncomeService incomeService;
+
+    @PostMapping
+    public ResponseEntity<IncomeDto> saveIncome(@RequestBody IncomeDto incomeDto){
+        return ResponseEntity.ok().body(incomeService.saveIncome(incomeDto));
+    }
+}
