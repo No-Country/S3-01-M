6e6a3543b00ec8beae3235edc03bcd348d6d1d1b
Gonzalo Altamirano <gonza12683@gmail.com>
2022-08-10 19:16:46 -0300

    Agrega CRUD front

---
+++ client/src/assets/icons/HappyFace.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HappyFace = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" role="img" width="40px" height="40px" viewBox="0 0 24 24" aria-labelledby="happyFaceIconTitle" stroke="#ffffff" strokeWidth="1.2" strokeLinecap="square" strokeLinejoin="miter" fill="none" color="#ffffff"> <title id="happyFaceIconTitle">Happy Face</title> <path d="M7.3010863,14.0011479 C8.0734404,15.7578367 9.98813711,17 11.9995889,17 C14.0024928,17 15.913479,15.7546194 16.6925307,14.0055328"/> <line strokeLinecap="round" x1="9" y1="9" x2="9" y2="9"/> <line strokeLinecap="round" x1="15" y1="9" x2="15" y2="9"/> <circle cx="12" cy="12" r="10"/> </svg>
+  )
+}
+
+export default HappyFace
---
+++ client/src/components/forms/Outcome.jsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import { useState } from 'react';
+import { useDispatch} from 'react-redux'
+import { addMovement } from '../../features/expenses/expensesSlice';
+import { nanoid } from '@reduxjs/toolkit';
+
+const Outcome = () => {
+   //Esto se va con Formik
+  const [amount, setAmount] = useState('')
+  const [category, setCategory] = useState('')
+  const [name, setName] = useState('')
+  const [date, setDate] = useState('')
+  const [description, setDescription] = useState('')
+  // ------------------------
+  const dispatch = useDispatch();
+  
+  const categories = ['Servicios', 'Vivienda', 'Salud', 'Limpieza', 'Impuestos', 'Alimentacion', 'Indumentaria', 'Cuidado personal', 'Entretenimientos']
+  
+  //Esto se va con Formik
+  const handleOutcomes = e=> setAmount(e.target.value)
+
+  const handleCategory = e=> setCategory(e.target.value)
+
+  const handleOutcomeName = e=> setName(e.target.value)
+
+  const handleDate = e => setDate(e.target.value)
+
+  const handleDescription = e => setDescription(e.target.value)
+// ------------------------
+
+  const submitOutcomes = (e)=>{
+      e.preventDefault;
+      const id= nanoid()
+      dispatch(addMovement({id,amount:-amount, category, name:name, date, description, type:'outcome'}))
+  }
+  return (
+    <>
+          {/* Título */}
+          <div className="flex items-center justify-center font-black m-3 mb-12">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10  text-red-600 animate-bounce"  role="img" width="48px" height="48px" viewBox="0 0 24 24" aria-labelledby="dolarIconTitle" stroke="#3ACC6F" strokeWidth="3" strokeLinecap="square" strokeLinejoin="miter" fill="none" color="#000"> <title id="dolarIconTitle">Dolar</title> <path d="M12 4L12 6M12 18L12 20M15.5 8C15.1666667 6.66666667 14 6 12 6 9 6 8.5 7.95652174 8.5 9 8.5 13.140327 15.5 10.9649412 15.5 15 15.5 16.0434783 15 18 12 18 10 18 8.83333333 17.3333333 8.5 16"/> </svg>
+              <h1 className="tracking-wide text-3xl text-gray-900">Agrega un gasto</h1>
+          </div>
+          {/* ---------Formulario-------- */}
+          <form className="flex flex-col justify-center">
+              <div className="flex justify-between items-center mb-3">
+                {/* Categorías */}
+                <div className="inline-flex items-center self-start">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mr-3 bg-gradient-to-r from-pink-600 to-red-600 shadow-lg rounded p-1.5 text-gray-100" role="img" width="24px" height="24px" viewBox="0 0 24 24" aria-labelledby="userIconTitle" stroke="#fff" strokeWidth="3" strokeLinecap="square" strokeLinejoin="miter" fill="none" color="#fff"> <title id="userIconTitle">User</title> <path strokeLinecap="round" d="M5.5,19.5 C7.83333333,18.5 9.33333333,17.6666667 10,17 C11,16 8,16 8,11 C8,7.66666667 9.33333333,6 12,6 C14.6666667,6 16,7.66666667 16,11 C16,16 13,16 14,17 C14.6666667,17.6666667 16.1666667,18.5 18.5,19.5"/> <circle cx="12" cy="12" r="10"/> </svg>
+                  <select className="font-bold text-gray-900" onChange={handleCategory}>
+                          {categories.map((categorie, index)=>
+                          <option key={categorie+index} value={categorie}>{categorie}</option>)
+                          }
+                  </select>
+                </div>
+                {/* Input egresos */}
+                <div className="flex">
+                    <button type="button"  className="bg-white p-1.5 font-bold rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z" clipRule="evenodd" />
+                    </svg>
+                    </button>
+                    
+                    <input  id="outcomes" placeholder='0' name="outcomes" type="number" onChange={handleOutcomes} className="max-w-[100px] font-bold font-mono py-1.5 px-2 mx-1.5
+                    block border border-gray-300 rounded-md text-sm shadow-sm  placeholder-gray-400
+                    focus:outline-none
+                    focus:border-sky-500
+                    focus:ring-1
+                    focus:ring-sky-500
+                    focus:invalid:border-red-500  focus:invalid:ring-red-500"/>
+                
+                    <button type="button"  className="bg-white p-1.5 font-bold rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
+                    </svg>
+                    </button>
+                </div>
+              </div>
+              {/* Nombre gasto */}
+              <label className="text-sm font-medium">Nombre</label>
+              <input onChange={handleOutcomeName} className="mb-3 px-2 py-1.5
+                  mb-3 mt-1 block w-full px-2 py-1.5 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
+                  focus:outline-none
+                  focus:border-sky-500
+                  focus:ring-1
+                  focus:ring-sky-500
+                  focus:invalid:border-red-500 focus:invalid:ring-red-500" type="text" name="name" placeholder="Supermercado"/>
+                {/* Fecha gasto */}
+                  <label className="text-sm font-medium">Fecha</label>
+                  <input onChange={handleDate} className="mb-3 px-2 py-1.5
+                  mb-3 mt-1 block w-full px-2 py-1.5 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
+                  focus:outline-none
+                  focus:border-sky-500
+                  focus:ring-1
+                  focus:ring-sky-500
+                  focus:invalid:border-red-500 focus:invalid:ring-red-500" type="date" name="outcomeDate" placeholder="Fecha"/>
+                {/* Descripción gasto */}
+                  <label className="text-sm font-medium">Descripción (opcional)</label>
+                  <textarea onChange={handleDescription} className="
+                  mb-3 mt-1 block w-full px-2 py-1.5 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
+                  focus:outline-none
+                  focus:border-sky-500
+                  focus:ring-1
+                  focus:ring-sky-500
+                  focus:invalid:border-red-500 focus:invalid:ring-red-500" name="messages" placeholder="Escribe algo"></textarea>
+          </form>
+          {/* ----------Termina formulario---------- */}
+          {/* Botón agregar */}
+          <button onClick={submitOutcomes} className="w-full px-4 py-1.5 rounded-md shadow-lg bg-gradient-to-r from-green-400 to-green-600 font-medium text-gray-100 block transition duration-300">
+              <span className="hidden">Sending</span>
+              <span >Agregar</span>
+          </button>
+    </>
+  )
+}
+
+export default Outcome
