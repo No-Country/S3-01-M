4a0c00cc61483b41be2de62d889f50e9b1ac8f86
Chelso <celsobrc@gmail.com>
2022-08-10 11:28:01 -0300

    Hotfix prod properties

--- BackEnd/fisalu/src/main/java/com/example/fisalu/exception/EmptyListException.java
+++
@@ -1,7 +0,0 @@
-package com.example.fisalu.exception;
-
-public class EmptyListException extends RuntimeException{
-    public EmptyListException(String message) {
-        super(message);
-    }
-}
