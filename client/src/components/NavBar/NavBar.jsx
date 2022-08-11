75ff29fc5bdf166942cfa9ff559225e17f7dabe1
mattiass <mattiasuru@gmail.com>
2022-08-11 14:20:20 -0300

    agrgado Link de router dom

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
     <div className="container flex flex-wrap justify-between items-center mx-auto">
-      <a href="/" className="flex items-center">
+      <Link to="/" className="flex items-center">
         <img src="../../../img/logo.png" className="" alt="Fisalu Logo" />
-      </a>
+      </Link>
       <button
         data-collapse-toggle="navbar-default"
         type="button"
@@ -31,37 +32,37 @@ const NavBar = () => {
       <div className="hidden w-full md:block md:w-auto" id="navbar-default">
         <ul className="flex flex-col items-center p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li>
-            <a
-              href="/"
+            <Link
+              to="/"
               className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
               aria-current="page"
             >
               Home
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/perfil"
+            <Link
+              to="/perfil"
               className="block px-5 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
             >
               Perfil
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/estadisticas"
+            <Link
+              to="/estadisticas"
               className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
             >
               Estadisticas
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/PanelGeneral"
+            <Link
+              to="/PanelGeneral"
               className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
             >
               Panel General
-            </a>
+            </Link>
           </li>
           <li>
             <button className="bg-red-500 hover:bg-red-700 text-white font-medium py-2 px-5 rounded-lg">
