dce2d20e4ec8f7ccfc5718dabc738f017a4bd71f
Pablo Vieyra <Pablin_vieyra1@outlook.com>
2022-09-03 05:54:47 -0300

    cambio de asd

--- client/src/components/Estadisticas/index.js
+++ client/src/components/Estadisticas/index.js
@@ -5,26 +5,29 @@ import CardMidEstadistic from "../CardEstadistic/CardMidEstadistic";
 import CardEstadistic from "../CardEstadistic/CardSmallEstadictic";
 import "./Estadisticas.css";
 import CardDetail from "../CardEstadistic/CardDetail";
-import {groupCategories, getMovements} from "../../features/expenses/expensesSlice";
-import background from "../Estadisticas/circle-scatter-haikei.svg"
-
+import {
+  groupCategories,
+  getMovements,
+} from "../../features/expenses/expensesSlice";
+import background from "../Estadisticas/circle-scatter-haikei.svg";
 
 const Estadisticas = () => {
-
-const movements = useSelector(getMovements)
-const movementsGroups = useSelector(groupCategories);
-// console.log(movementsGroups)
-// const movKeys = Object.keys(movementsGroups);
-const titles = Object.keys(movementsGroups);
-const categoryList = Object.values(movementsGroups);
-// console.log(movKeys)
-// movKeys.map((category)=> console.log(movementsGroups[category]))
-// const movementsList = movKeys.map((category)=>movementsGroups[category])
-// console.log(movementsList)
-
+  const movements = useSelector(getMovements);
+  const movementsGroups = useSelector(groupCategories);
+  // console.log(movementsGroups)
+  // const movKeys = Object.keys(movementsGroups);
+  const titles = Object.keys(movementsGroups);
+  const categoryList = Object.values(movementsGroups);
+  // console.log(movKeys)
+  // movKeys.map((category)=> console.log(movementsGroups[category]))
+  // const movementsList = movKeys.map((category)=>movementsGroups[category])
+  // console.log(movementsList)
 
   return (
-    <div className="pt-[120px] min-h-screen md:px-48 flex flex-row justify-evenly py-6 flex-wrap lg:flex-nowrap min-w-[50%] bg-cover" style={{backgroundImage:`url(${background})`}}>
+    <div
+      className="pt-[120px] min-h-screen md:px-48 flex flex-row justify-evenly py-6 flex-wrap lg:flex-nowrap min-w-[50%] bg-cover"
+      style={{ backgroundImage: `url(${background})` }}
+    >
       <section>
         <div className="flex flex-row gap-6 flex-wrap">
           <div className="mx-auto max-w-[400px]">
@@ -39,9 +42,17 @@ const categoryList = Object.values(movementsGroups);
         </div>
       </section>
       <section className="flex flex-row flex-wrap justify-center gap-6 content-start pb-8">
-          {
-            categoryList.length>=1 ? categoryList.map((category, index)=>(<CardEstadistic key={category+index} expense={category} title={titles[index]}/>)) : <>asd</>
-          }
+        {categoryList.length >= 1 ? (
+          categoryList.map((category, index) => (
+            <CardEstadistic
+              key={category + index}
+              expense={category}
+              title={titles[index]}
+            />
+          ))
+        ) : (
+          <></>
+        )}
       </section>
     </div>
   );
