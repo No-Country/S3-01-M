0bd9319c565a2e412e117fb8f2971cb1884356e6
Gonzalo Altamirano <gonza12683@gmail.com>
2022-09-03 11:47:25 -0300

    Agrega lógica para charts

--- client/src/components/CardEstadistic/CardBigEstadistic.js
+++ client/src/components/CardEstadistic/CardBigEstadistic.js
@@ -1,12 +1,25 @@
 import React from "react";
 import LineChart from "../Graficos/LineChart";
+import { useSelector } from "react-redux";
 
 const CardBigEstadistic = () => {
+  const incomes = useSelector(state=> state.incomes.incomes)
+  // const incomesAmount = incomes.map(income=> income.amount)
+  console.log(incomes)
+  // console.log(incomesAmount)
+
+  const groups = incomes.slice().sort((a,b)=>a.date.localeCompare(b.date)).reduce((groups, item) => {
+      const group = (groups[item.date] || []);
+      group.push(item);
+      groups[item.date] = group;
+      return groups;
+    }, {});
+
   return (
-    <div className="bg-white md:p-8 mx-auto rounded-md text-slate-50 border shadow-md shadow-indigo-500/50">
-      <h1 className="text-3xl text-gray-800"> CardBigEstadistic</h1>
-      <div className="flex flex-row justify-start">
-        <LineChart />
+    <div className="bg-white mx-auto rounded-md text-slate-50 shadow-md shadow-indigo-500/50 w-full">
+      <h2 className="text-2xl text-white bg-[#5146ED] w-full text-center py-2">Ingresos</h2>
+      <div className="flex flex-row justify-start px-8 py-4">
+        <LineChart groups={groups}/>
       </div>
     </div>
   );
--- client/src/components/CardEstadistic/CardMidEstadistic.js
+++ client/src/components/CardEstadistic/CardMidEstadistic.js
@@ -1,12 +1,22 @@
 import React from "react";
 import LineChart from "../Graficos/LineChart";
+import { useSelector } from "react-redux";
 
 const CardMidEstadistic = () => {
+  const outcomes = useSelector(state=> state.outcomes.outcomes)
+
+  const groups = outcomes.slice().sort((a,b)=>a.date.localeCompare(b.date)).reduce((groups, item) => {
+      const group = (groups[item.date] || []);
+      group.push(item);
+      groups[item.date] = group;
+      return groups;
+    }, {});
+
   return (
-    <div className="bg-white mx-auto md:p-8 rounded-md text-slate-50 shadow-md shadow-indigo-500/50">
-      <h1 className="text-3xl text-gray-800">Gastos</h1>
-      <div className="flex flex-row justify-start">
-        <LineChart/>
+    <div className="bg-white mx-auto rounded-md text-slate-50 shadow-md shadow-indigo-500/50 w-full">
+      <h2 className="text-2xl text-white bg-[#5146ED] w-full text-center py-2">Gastos</h2>
+      <div className="flex flex-row justify-start px-8 py-4">
+        <LineChart groups={groups}/>
       </div>
     </div>
   );
